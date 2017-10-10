51cfcb2c5442bdb9e97fa26e503cab66b25cb86a
Bengt Rutisson <bengt.rutisson@looklet.com>
2017-10-10 20:28:43 +0200

    Initial load

---
+++ src/ggrtabell.js
@@ -0,0 +1,31 @@
+
+var x = 5;
+var y = 6;
+
+var correctCount = 0;
+var incorrectCount = 0;
+
+function updateValues() {
+    x = Math.floor((Math.random() * 10) + 1);;
+    y = Math.floor((Math.random() * 10) + 1);;
+    $("#valueLabel").text(x + " * " + y + " = ");
+    $("#answer").val("");
+}
+
+function saveResult() {
+    var result = "<li>" + x + " * " + " " + y + " = " + (x * y) + "</li>";
+    $("#results").prepend(result);
+}
+
+function checkAnswer() {
+    var answer = $("#answer").val();
+    if (answer == x * y) {
+        correctCount++;
+        saveResult();
+        updateValues();
+    } else {
+        incorrectCount++;
+        alert("Hoppsan, fel svar. Försök igen.");
+    }
+    $("#count").html(correctCount + " rätta svar / " + incorrectCount + " fel svar");
+}
